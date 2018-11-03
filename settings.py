1254b97510031c8483ae263db8d4c6c0def6f93e
I-mm <36660785+I-mm@users.noreply.github.com>
2018-11-03 13:13:33 +0800

    1st commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+DBENGINE = 'sqlite3'  # ENGINE OPTIONS: mysql, sqlite3, postgresql
+DBNAME = 'beijing_xicheng.db'
+DBUSER = 'root'
+DBPASSWORD = '123456'
+DBHOST = '127.0.0.1'
+DBPORT = 3306
+CITY = 'bj'  # only one, shanghai=sh shenzhen=sh......
+REGIONLIST = [u'xicheng']  # only pinyin support
