0a212f352dc1cad07032bf1689d72084b9c1fd38
I-mm <36660785+i-mm@users.noreply.github.com>
2018-11-04 13:47:26 +0800

    houseinfo.csv (to 9300th house)

--- Untitled Folder/readCSV.py
+++ Untitled Folder/readCSV.py
@@ -7,15 +7,21 @@ with open(DBcsvName + '.csv', 'r', encoding='UTF-8') as f:
     lines = f.readlines()
     print(lines[0])
 
-with open(DBcsvName + '.txt', 'w', encoding='UTF-8') as fw:
+with open(DBcsvName + '_readable.csv', 'w', encoding='UTF-8') as fw:
+    fw.write(
+        "houseID" + "," + "title" + "," + "link" + "," + "community" + "," + "years" + "," + "housetype" + "," + "square" + "," + "direction" + "," + "floor" + "," + "taxtype" + "," + "totalPrice" + "," + "unitPrice" + "," + "followInfo" + "," + "decoration" + "," + "validdate" + "," + "coor_x,y")
+    fw.write('\n')
     i = 1
     for line in lines:
-        line = line.split("|")  # 将单个数据分隔开存好
+        line = line.strip('\n').split("|")  # 将单个数据分隔开存好
         addr = line[3]
         coor = getCoordinate.get_addrCoor(addr)
-        line.append(coor)
+        line.append('\"' + coor + '\"')
         for e in line:
-            fw.write(e + '\t')
+            if e == line[0]:
+                fw.write(e)
+            else:
+                fw.write(',' + e)
         fw.write('\n')
         print("count: " + str(i))
         i = i + 1
